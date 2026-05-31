47f71069a11326bea9901cae5efd5e904bee8a04
landpage101-oss <landpage101@gmail.com>
2026-05-31 20:43:34 +0300

    feat(sources): expand docs_python_org seeds from 7 to 27 URLs

--- src/sources/docs_python_org.py
+++ src/sources/docs_python_org.py
@@ -3,6 +3,7 @@ from collections.abc import Iterable
 _BASE = "https://docs.python.org/3/"
 
 _SEEDS = [
+    # --- already canonical ---
     _BASE + "library/json.html",
     _BASE + "library/typing.html",
     _BASE + "library/asyncio.html",
@@ -10,6 +11,32 @@ _SEEDS = [
     _BASE + "library/re.html",
     _BASE + "library/pathlib.html",
     _BASE + "library/dataclasses.html",
+    # --- concurrency ---
+    _BASE + "library/threading.html",
+    _BASE + "library/multiprocessing.html",
+    _BASE + "library/concurrent.futures.html",
+    _BASE + "library/queue.html",
+    # --- data structures & functional ---
+    _BASE + "library/collections.html",
+    _BASE + "library/itertools.html",
+    _BASE + "library/functools.html",
+    _BASE + "library/heapq.html",
+    # --- file & os ---
+    _BASE + "library/os.html",
+    _BASE + "library/shutil.html",
+    _BASE + "library/io.html",
+    _BASE + "library/contextlib.html",
+    # --- serialization & config ---
+    _BASE + "library/csv.html",
+    _BASE + "library/pickle.html",
+    _BASE + "library/configparser.html",
+    # --- date/time & numeric ---
+    _BASE + "library/datetime.html",
+    _BASE + "library/decimal.html",
+    _BASE + "library/statistics.html",
+    # --- introspection & testing ---
+    _BASE + "library/inspect.html",
+    _BASE + "library/unittest.html",
 ]
 
 
